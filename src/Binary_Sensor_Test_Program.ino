a11caf266d54d33bd89c9af4571da4e7cfe82ad0
Jørgen Bo Madsen <99587453+jebmdk@users.noreply.github.com>
2025-01-18 18:10:28 +0100

    Add files via upload

---
+++ src/Binary_Sensor_Test_Program.ino
@@ -0,0 +1,29 @@
+/*
+Simple program for use with Infrared binary reflective sensor.
+FC-51, SEN0503, SEN0523, FE18-D50NK and E18-D80NK. All Open Collector output.
+
+Jørgen Bo Madsen - V1 - September 2024 - dgmf@jorgen-madsen.dk
+-------------------------------------------------------------------------------------------------
+*/
+
+const byte Sensor_Pin = 16;  // Digital D2 (no 5)
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(Sensor_Pin, INPUT_PULLUP);
+}
+
+
+void loop() {
+  Serial.print(millis());
+  if (digitalRead(Sensor_Pin) == LOW) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.println(": Object detekted!"); 
+  }
+  else {
+    digitalWrite(LED_BUILTIN, LOW);
+    Serial.println(": Waiting for an object to reflect the IR beam!");
+  }
+  delay(100);
+}
